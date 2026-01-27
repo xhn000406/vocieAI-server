6fca109d412032e7046150a347e7e16992302de6
xhn0000406 <1127835255@qq.com>
2026-01-27 23:56:41 +0800

    feat：数据库文件修改

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,6 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`
+const prisma = new PrismaClient({} as any);
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+export { prisma };
